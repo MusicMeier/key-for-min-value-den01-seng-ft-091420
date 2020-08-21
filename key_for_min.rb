74c541139502aff4490104d896259e6883b238e8
Music Meier <musicm987@gmail.com>
2020-08-21 19:22:28 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,8 +5,9 @@ def key_for_min_value(name_hash)
   lowest_key = nil
   lowest_value = nil
   name_hash.each do |key, value|
-  if lowest_value == key
-  
+  if  value < lowest_value
+  lowest_key = key
+  lowest_value = value
   end
   
   end
